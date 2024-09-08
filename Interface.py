a7b3cc0c94194dfd620c0f4a2f523dc6b62e4dc6
lbtriana <lb.triana@uniandes.edu.co>
2024-09-08 10:29:47 -0500

    Esconde el logo de git hub y el menu

--- Interface.py
+++ Interface.py
@@ -16,22 +16,14 @@ import plotly.express as px
 #=========================Configuración de la página============================
 st.set_page_config(layout="wide")
 
-#Esconde el menú
+#Esconde el menú y el icono de git hub
 hide = """
     <style>
         #MainMenu {visibility: hidden;}
+        #GithubIcon {visibility: hidden;}
     </style>
 """
 st.markdown(hide, unsafe_allow_html=True)
-
-#Esconde el enlace al repositorio de Git hub
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
 
 col_1, col_2, col_3 = st.columns(3)
 
