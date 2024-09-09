2f9e0f3a095fc5f62f9c6196a6705729636cab63
lbtriana <lb.triana@uniandes.edu.co>
2024-09-08 21:02:37 -0500

    prueba colores

--- Interface.py
+++ Interface.py
@@ -16,14 +16,6 @@ import plotly.express as px
 #=========================Configuración de la página============================
 st.set_page_config(layout="wide")
 
-st.markdown("""
-    <style>
-    :root {
-        --primary-color: #1f5e9e;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
 #Versión final para esconder el menú y stToolbar 
 hide_streamlit_style = """
 <style>
@@ -962,9 +954,9 @@ if authenticate_user():
         st.markdown('<h3 style="font-size:18px;">Acciones</h3>', unsafe_allow_html=True)
     
         respuesta_pressed, ayuda_pressed, repetir_pressed, nuevo_pressed = st.columns(4)
-        respuesta_clicked = respuesta_pressed.button("Verificar respuesta", type="primary", key=f"respuesta_button_{st.session_state.pregunta_actual}", help="Verificación de la respuesta", use_container_width=True)
+        respuesta_clicked = respuesta_pressed.button(":blue:[Verificar respuesta]", key=f"respuesta_button_{st.session_state.pregunta_actual}", help="Verificación de la respuesta", use_container_width=True)
         ayuda_clicked = ayuda_pressed.button("Ayuda", type="primary", key=f"ayuda_button_{st.session_state.pregunta_actual}", help="Ayuda para la solución", use_container_width=True)
-        repetir_pressed.button("Nueva versión", key="nueva_version_button", help="Genera una nueva versión del problema", use_container_width=True, on_click=nueva_version_callback)
+        repetir_pressed.button(":blue-background:[Nueva versión]", key="nueva_version_button", help="Genera una nueva versión del problema", use_container_width=True, on_click=nueva_version_callback)
         nuevo_pressed.button("Siguiente problema", key=f"nuevo_problema_button{st.session_state.pregunta_actual}", help="Genera un nuevo problema", use_container_width=True, on_click=nuevo_problema_callback)
     
         if st.session_state.get("consent", False):
